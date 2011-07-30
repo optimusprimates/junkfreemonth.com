37f791193e2f22a9177073c0d3e98f2b86d383ab
Matthew Scott <gldnspud@gmail.com>
2011-07-30 01:41:05 -0500

    Add a simple index to get around silly ISP redirect-on-404 stuff

--- urls.py
+++ urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
